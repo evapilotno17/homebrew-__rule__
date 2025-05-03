e4e1e2ddaeabab3eb2f3de0e4f8a74b46eee5eb6
rei <evapilotno17@nerv.org>
2025-05-04 03:48:52 +0530

    rule

--- Formula/rule.rb
+++ Formula/rule.rb
@@ -1,4 +1,4 @@
-class _Rule__ < Formula
+class Rule < Formula
     desc      "print 1d2state3neighbor cellular automata to your terminal. it looks cool ;)"
     homepage  "https://github.com/evapilotno17/__rule__"
     url       "https://github.com/evapilotno17/__rule__/archive/refs/tags/v1.tar.gz"
